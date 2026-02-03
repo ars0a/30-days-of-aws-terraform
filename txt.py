cf881ceccc26582ce76fce1358f8f277652979fa
Aditya Raj Singh <adityaraj.singh.ars@gmail.com>
2026-02-03 21:36:43 +0530

    initial commit

---
+++ txt.py
@@ -0,0 +1,13 @@
+# print ("hello world")
+
+# print odd numbers from 1 to 20 with while loop
+
+# a = 1
+# total = 0
+# while a <= 20:
+#     if a % 2 != 0:
+#         total = total + 1
+#         a = a + 1
+#     else:
+#         a = a + 1
+# print (total)
